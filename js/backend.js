ca34d73649f6c3320d4fc641c44828defeeec1d0
Mario Neubacher <neubacher.mario00@gmail.com>
2021-11-12 17:18:49 +0100

    backend

---
+++ js/backend.js
@@ -0,0 +1,9 @@
+setURL('http://developerakademie.com/smallest_backend_ever');
+let user = [];
+
+async function init() {
+    await downloadFromServer();
+    users = JSON.parse(backend.getItem('users')) || [];
+
+    includeHTML();
+}
