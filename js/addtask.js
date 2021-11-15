098c86a645242c4831c8a49f6fe9d50fa4bc29be
Mario Neubacher <neubacher.mario00@gmail.com>
2021-11-15 12:11:10 +0100

    selection function and browser tab icon added

--- js/addtask.js
+++ js/addtask.js
@@ -13,6 +13,8 @@ Members = [
     },
 ];
 
+let selectedMembers = [];
+
 /**
  * Shows the profile picture and name of the coworker with a json array
  */
@@ -26,8 +28,27 @@ function assignToMembers() {
             <tr>
                 <td><img class="img-profilePicture" src="${Member['img']}"></td>
                 <td>${Member['name']}</td>
-                <td><img class="img-add" src="img/add.png"></td>
+                <td><img onclick="select(${i})" id="id-add${i}" class="img-add" src="img/add.png"></td>
             </tr>
         `;
     }
+}
+
+/**
+ * Alters the selectedMembers array and replaces the plus with a minus by selection
+ * @param {number} i - filters which user was selected
+ */
+function select(i) {
+    let position = selectedMembers.indexOf(Members[i]);
+    if (noMemberSelected(position)) {
+        selectedMembers.push(Members[i]);
+        document.getElementById('id-add'+i).src="img/remove.png";
+    } else {
+        selectedMembers.splice(position, 1);
+        document.getElementById('id-add'+i).src="img/add.png";
+    }
+}
+
+function noMemberSelected(position){
+    return position == -1;
 }
