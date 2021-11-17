8d5aa2090b13f25222d8817304235763fe4c04d2
atnbn <86778371+atnbn@users.noreply.github.com>
2021-11-17 14:18:35 +0100

    backlog js eingefügt

--- js/backlog.js
+++ js/backlog.js
@@ -5,5 +5,41 @@
     await downloadFromServer();
     loadAllTasks();
     includeHTML();
+    loadTasks();
 }
 
+
+
+
+
+
+
+
+function loadTasks() {
+    let taskRow = document.getElementById('taskRow');
+    taskRow.innerHTML = '';
+    for (let i = 0; i < allTasks.length; i++) {
+
+        console.log('showsomething');
+
+        taskRow.innerHTML += `
+        <div class="history">
+            <img class="member-pic" src=${Member['img']}>
+
+            <div class="name-email">
+                <p>${Member['name']}</p>
+                <p class="email">${Member['email']}</p>
+            </div>
+
+            <div class="category">
+                <p class="category1">${urgency}</p>
+            </div>
+
+            <div class="details-history">
+                <p>${loadAllTasks[i][description]}</p>
+            </div>
+        </div>
+        `;
+
+    }
+}
