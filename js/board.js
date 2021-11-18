10045e7c0eb81ed283bb237aefe120d7152a7b61
Melanie Kell <melanie.kell@gmx.de>
2021-11-18 13:42:23 +0100

    few changes trying to load tasks in board

---
+++ js/board.js
@@ -0,0 +1,33 @@
+/**
+ * Fires the functions when side loads
+ */
+async function init() {
+    await downloadFromServer();
+    loadAllTasks();
+    includeHTML();
+    loadTasks();
+}
+
+/* Array für the tasks from the backend */
+let todos = [];
+
+/* trying to load the tasks from backend into todos array */
+async function tasksForBoard() {
+    await downloadFromServer();
+    todos = JSON.parse(backend.getItem('todos')) || [];
+}
+
+function renderTasks() {
+
+    for (let i = 0; i < todos.length; i++) {
+        document.getElementById('boardColumn').innerHTML += ` 
+
+    <div class="task-board"> 
+        <div><b>${todos[i].title}</b></div>
+        <div>${todos[i].date}</div>
+        <div>${todos[i].category}</div>
+    </div>`;
+    }
+
+
+}
